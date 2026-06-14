2aba7c5b2f15b99746da2d98bd4f8bc3fe3bd817
damusss <ricciardi.damiano06@gmail.com>
2026-06-14 17:07:13 +0200

    Move mold buildings to separate file

--- src/server/buildings/__init__.py
+++ src/server/buildings/__init__.py
@@ -1,5 +1,6 @@
 from src.server.buildings.bot import Bot
 from src.server.buildings.misc import Lamp
+from src.server.buildings.mold import MoldSanitizer, MoldHarvester, MoldMiner
 from src.server.buildings.mining import NyliumHarvester, Miner
 from src.server.buildings.storing import Storage, Hopper
 from src.server.buildings.crafting import Crafter
